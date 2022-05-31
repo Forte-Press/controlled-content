b99abff10f4ef31b26ca8a054a39cfb0f7ba0c14
Phil Webster <pwebster33@gmail.com>
2022-05-31 07:57:32 -0400

    Release 0.2.0 (#5)
    * Block side-bar now has the ability to enter custom placeholder (#3)
    
    * Add instructions to block
    
    * Update placeholder color
    
    * Textinput now placeholder
    
    * Add docs link to plugin action links (#4)
    
    * Update version to 0.2.0
    
    * Update changelog

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'be61761fa88cf0d6642cb5649e55fe06');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '208119ac1e3342d3891c3459aebd27f4');
--- controlled-content.php
+++ controlled-content.php
@@ -4,7 +4,7 @@
  * Description:       A block that restricts the allowed block types for a content section.
  * Requires at least: 5.8
  * Requires PHP:      7.0
- * Version:           0.1.0
+ * Version:           0.2.0
  * Author:            wpblockbuddy
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
@@ -31,4 +31,18 @@ function wpblockbuddy_controlledContentVar () {
 		'blockChoices' => apply_filters('wpblockbuddy_controlled_content_block_choices', $blockChoices)
 	];
 	wp_localize_script('wpblockbuddy-controlled-content-editor-script', 'wpBlockBuddyControlledContent', $args);
+}
+
+add_action('init', 'wpblockbuddy_cc_setup_plugin_action');
+function wpblockbuddy_cc_setup_plugin_action() {
+ 	$basename = plugin_basename(__FILE__);
+	add_filter( 'plugin_action_links_' . $basename, 'wpblockbuddy_cc_action_links');
+}
+
+function wpblockbuddy_cc_action_links($links) {
+	$additional_links = array(
+		'profiles'   => sprintf('<a href="%s" style="font-weight: 700;" target="_blank" rel="NOFOLLOW">%s</a>', "https://wpblockbuddy.com/blocks/controlled-content-block/?source=action-link", __('Docs', 'wp-migrate-db')),
+	);
+
+	return $links + $additional_links;
 }
